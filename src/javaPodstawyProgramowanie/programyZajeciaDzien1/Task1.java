8477a4e3bfff24fc2506d3c0fc178c5b13f16917
Tomek <wartego@wp.pl>
2022-09-17 11:10:08 +0200

    Pierwszy program

--- src/javaPodstawyProgramowanie/programyZajeciaDzien1/Task1.java
+++ src/javaPodstawyProgramowanie/programyZajeciaDzien1/Task1.java
@@ -3,12 +3,33 @@ package javaPodstawyProgramowanie.programyZajeciaDzien1;
 public class Task1 {
     public static void main(String[] args) {
         displayWordUsingDecimalFormat();
+        System.out.println("");
+        displayWordUsingBinaryFormat();
+        System.out.println("");
+        displayWordUsingHexaFormat();
     }
 
     private static void displayWordUsingDecimalFormat() {
         char s = 83; // "ZNAK "S" w ASCII to 83
         char d = 68;
         char a = 65;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+
+    private static void displayWordUsingBinaryFormat() {
+        char s =  0b01010011; // "ZNAK "S" w ASCII jako BINARY "0b" rozpoznaje jako binary
+        char d =  0b01000100;
+        char a =  0b01000001;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+    private static void displayWordUsingHexaFormat() {
+        char s =  0x53; // "ZNAK "S" w ASCII jako BINARY "0b" rozpoznaje jako binary
+        char d =  0x44;
+        char a =  0x41;
         System.out.print(s);
         System.out.print(d);
         System.out.print(a);
