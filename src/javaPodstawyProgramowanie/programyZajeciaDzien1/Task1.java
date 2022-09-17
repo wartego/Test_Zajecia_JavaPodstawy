8977c4ddb19d818f2826ca09c6d97f477be6b8d4
Tomek <wartego@wp.pl>
2022-09-17 10:56:40 +0200

    Pierwszy program

--- src/javaPodstawyProgramowanie/programyZajeciaDzien1/Task1.java
+++ src/javaPodstawyProgramowanie/programyZajeciaDzien1/Task1.java
@@ -2,6 +2,15 @@ package javaPodstawyProgramowanie.programyZajeciaDzien1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("here");
+        displayWordUsingDecimalFormat();
+    }
+
+    private static void displayWordUsingDecimalFormat() {
+        char s = 83; // "ZNAK "S" w ASCII to 83
+        char d = 68;
+        char a = 65;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
     }
 }
